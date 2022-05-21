c143f11c52f3c9e6233611994d75ad70f2d98d3b
austin <business.mocksock@gmail.com>
2022-05-21 09:03:01 -0500

    add logic to see if debugger works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { areAllEquivalent } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcli-vscode-debug';
+
+  debuggerValueTripped: boolean = false
+  
+  constructor() {
+    this.checkDebugValue();
+  }
+
+  ngOnInit() {
+    return this.debuggerValueTripped = true 
+  }
+
+  checkDebugValue() {
+    if(this.debuggerValueTripped == true) {
+      console.log("The Code was Executed");
+    }
+  }
 }
