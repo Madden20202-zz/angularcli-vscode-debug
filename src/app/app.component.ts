ed22beb200bddb7415906df631db97c48d8f37d7
austin <business.mocksock@gmail.com>
2022-05-21 09:09:09 -0500

    add documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularcli-vscode-debug';
 
+  // Basic boolean to be changed
   debuggerValueTripped: boolean = false
   
   constructor() {
+    // Value changed on construct
     this.checkDebugValue();
   }
 
   ngOnInit() {
+    // Once widget initialized, default value is changed
     return this.debuggerValueTripped = true 
   }
 
+  // basic method to check if value actually changed
   checkDebugValue() {
     if(this.debuggerValueTripped == true) {
       console.log("The Code was Executed");
